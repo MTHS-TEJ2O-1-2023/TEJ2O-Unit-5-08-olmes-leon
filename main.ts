c96f9267c0ce83f2a99ed4126229f11931578579
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-11-29 13:07:52 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Olmes
+ * Created on: Nov 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+
+let distansetoObject: number = 0
+
+// loop forever
+while (true) {
+    if (input.buttonIsPressed(Button.A) == true) {
+        basic.showIcon(IconNames.Duck)
+        while (true) {
+         robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+        
+        }
+    }
+
+    if (input.buttonIsPressed(Button.B) == true) {
+        // move car forwards and backwards
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(10, 48)
+        basic.pause(500)
+        robotbit.StpCarMove(-10, 48)
+        basic.showIcon(IconNames.Diamond)
+    }
+}
